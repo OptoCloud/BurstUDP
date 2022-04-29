8a1025a4b5f523204d17a46c1c24c849ef3eee40
OptoCloud <26223094+OptoCloud@users.noreply.github.com>
2022-04-29 22:57:54 +0200

    Even more comments

--- Program.cs
+++ Program.cs
@@ -38,20 +38,29 @@ namespace BurstUDP
         /// <returns></returns>
         static IEnumerable<FileChunk> ReadFileChunks(Guid fileID, string fileName)
         {
+            // For debugging
             ulong nChunks = 0;
+
+            // Open file for reads
             using (FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
             {
                 uint chunkId = 0;
+
+                // While there is still data to read
                 while (fileStream.Position != fileStream.Length)
                 {
+                    // Get a array from the array pool to keep the memory usage down, and the speed up
                     byte[] packet = arrayPool.Rent(Constants.PacketMaxSize);
 
+                    // Read the data into the array
                     var nRead = fileStream.Read(packet, Constants.FileChunkPacketOffset, Constants.FileChunkSize);
                     if (nRead <= 0)
                         break;
 
+                    // For debugging
                     nChunks++;
 
+                    // If we read less than the chunk size, then its probably the last chunk, resize it to the actual size
                     if (nRead < Constants.FileChunkSize)
                     {
                         byte[] partialPacket = new byte[nRead];
@@ -59,6 +68,7 @@ namespace BurstUDP
                         packet = partialPacket;
                     }
 
+                    // Queue the chunk for further processing
                     yield return new FileChunk(fileID, chunkId++, packet);
                 }
             }
@@ -81,21 +91,24 @@ namespace BurstUDP
             {
                 using (UdpClient udpClient = new UdpClient())
                 {
+                    // For testing
                     udpClient.Connect("127.0.0.1", 12345);
 
+                    // Iterate through all the chunks
                     foreach (var fileChunk in fileChunks)
                     {
-                        // Set header
+                        // Set the header ( message ID, file ID, and chunk ID )
                         fileChunk.Data[0] = (byte)1;
                         Array.Copy(fileChunk.FileID.ToByteArray(), 0, fileChunk.Data, 1, Constants.GuidSize);
                         Array.Copy(BitConverter.GetBytes(fileChunk.ChunkID), 0, fileChunk.Data, 1 + Constants.GuidSize, sizeof(uint));
 
-                        // Hash the chunk
+                        // Hash the the header + filechunk data
                         byte[] hash = sha256.ComputeHash(fileChunk.Data, 0, fileChunk.Data.Length - Constants.HashSize);
 
                         // Copy the hash to the end of the packet
                         Array.Copy(hash, 0, fileChunk.Data, fileChunk.Data.Length - Constants.HashSize, Constants.HashSize);
 
+                        // Send the packet
                         await udpClient.SendAsync(fileChunk.Data, fileChunk.Data.Length);
                     }
                 }
